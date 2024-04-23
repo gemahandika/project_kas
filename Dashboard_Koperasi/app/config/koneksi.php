e1cdce2ef084cd9053f40d0678a4b4f577e9ad30
gemahandika <gemahandika88@gmail.com>
2024-04-23 13:35:17 +0700

    config db

--- Dashboard_Koperasi/app/config/koneksi.php
+++ Dashboard_Koperasi/app/config/koneksi.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Jakarta');
 
 
 // koneksi database
-$koneksi = mysqli_connect('localhost', 'u568977811_kreasi', 'Kreasianugerah.123', 'u568977811_kreasi');
+$koneksi = mysqli_connect('localhost', 'u568977811_kreasi', 'KopKas2024', 'u568977811_kreasi');
 if (mysqli_connect_errno()) {
 	echo mysqli_connect_error();
 }
