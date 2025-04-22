3214164e7d7f3554163f531b5d0cfa0784e34020
gema_handika <gemahandika88@gmail.com>
2025-04-22 23:54:10 +0700

    Laporan

--- Dashboard_Koperasi/app/config/koneksi.php
+++ Dashboard_Koperasi/app/config/koneksi.php
@@ -10,13 +10,13 @@ date_default_timezone_set('Asia/Jakarta');
 // }
 
 // // koneksi database Local
-$koneksi = mysqli_connect('localhost', 'root', '', 'u568977811_kreasi');
-if (mysqli_connect_errno()) {
-	echo mysqli_connect_error();
-}
-
-// // koneksi database jne
-// $koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_dbkas');
+// $koneksi = mysqli_connect('localhost', 'root', '', 'u568977811_kreasi');
 // if (mysqli_connect_errno()) {
 // 	echo mysqli_connect_error();
 // }
+
+// // koneksi database jne
+$koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_dbkas');
+if (mysqli_connect_errno()) {
+	echo mysqli_connect_error();
+}
