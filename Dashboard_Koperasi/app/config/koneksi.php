5a0170bf01c033c0a63e838c2fa991a3188912fb
gema_handika <gemahandika88@gmail.com>
2025-04-22 22:06:50 +0700

    update

--- Dashboard_Koperasi/app/config/koneksi.php
+++ Dashboard_Koperasi/app/config/koneksi.php
@@ -4,13 +4,19 @@ date_default_timezone_set('Asia/Jakarta');
 
 
 // koneksi database public
-$koneksi = mysqli_connect('localhost', 'u619550363_kreasianugerah', 'Kas2024@', 'u619550363_db_kas');
-if (mysqli_connect_errno()) {
-	echo mysqli_connect_error();
-}
+// $koneksi = mysqli_connect('localhost', 'u619550363_kreasianugerah', 'Kas2024@', 'u619550363_db_kas');
+// if (mysqli_connect_errno()) {
+// 	echo mysqli_connect_error();
+// }
 
 // // koneksi database Local
 // $koneksi = mysqli_connect('localhost', 'root', '', 'u568977811_kreasi');
 // if (mysqli_connect_errno()) {
 // 	echo mysqli_connect_error();
 // }
+
+// // koneksi database jne
+$koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_dbkas');
+if (mysqli_connect_errno()) {
+	echo mysqli_connect_error();
+}
